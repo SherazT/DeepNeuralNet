2057b3bbc2a119fd05d1392ee081003448141ac6
Sheraz Tariq <sheraztariq@hotmail.com>
2017-01-23 03:57:30 -0500

    completed neural net model

--- deep-neural-net.py
+++ deep-neural-net.py
@@ -8,13 +8,39 @@ no_nodes_hlyr2 = 500
 no_nodes_hlyr3 = 500
 
 n_classes = 10 #outputs
+n_inputs = 784
 batch_size = 100
 
 #height x width
-x = tf.placeholder('float', [None,784]) #28x28 = 784 pixels (squashed)
+x = tf.placeholder('float', [None,n_inputs]) #28x28 = 784 pixels (squashed)
 y = tf.placeholder('float')
 
 def neural_network_model(data):
-	hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([784,  no_nodes_hlyr1])),
+	# (input_data * weights) + biases
+	hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([n_inputs,  no_nodes_hlyr1])),
 					  'biases': tf.Variable(tf.random_normal(no_nodes_hlyr1))}
+
+	hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([no_nodes_hlyr1,  no_nodes_hlyr2])),
+					  'biases': tf.Variable(tf.random_normal(no_nodes_hlyr2))}
+
+	hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([no_nodes_hlyr2,  no_nodes_hlyr3])),
+					  'biases': tf.Variable(tf.random_normal(no_nodes_hlyr3))}
+
+	output_layer = {'weights': tf.Variable(tf.random_normal([no_nodes_hlyr3, n_classes])),
+					  'biases': tf.Variable(tf.random_normal(n_classes))}
+
+	layer1 = tf.add(tf.matmul(data, hidden_layer_1['weights']) + hidden_layer_1['biases'])
+	layer1 = tf.nn.relu(layer1)
+
+	layer2 = tf.add(tf.matmul(layer1, hidden_layer_2['weights']) + hidden_layer_2['biases'])
+	layer2 = tf.nn.relu(layer2)
+
+	layer3 = tf.add(tf.matmul(layer2, hidden_layer_3['weights']) + hidden_layer_3['biases'])
+	layer3 = tf.nn.relu(layer3)
+
+	output = tf.matmul(layer3, output_layer['weights']) + output_layer['biases']
+
+	return output
+
+
 
